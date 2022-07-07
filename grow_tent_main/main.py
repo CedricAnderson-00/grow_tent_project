95231b84669e3a3d3fd52be8cd3043e6cd0140c6
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-06 22:37:14 -0500

    make SQL query templates

--- grow_tent_main/main.py
+++ grow_tent_main/main.py
@@ -3,18 +3,21 @@
 import grow_tent_main
 import grow_tent_main.TempHumiditySensor
 from grow_tent_main.views.LcdDisplay import lcd
-from grow_tent_main.MySQL.mysql_main import connect
+from grow_tent_main.MySQL.mysql_main import database
 
 test = str((grow_tent_main.TempHumiditySensor.temp_f))
 humidity = str((grow_tent_main.TempHumiditySensor.hum))
 
-# empty dictionary to send to database
-sql_dict = {'plant_id': 1, 'temp_f': 00.0, 'temp_c': 00.0, 'humidity': 00,
-            'daily_water': 00.0, 'light_on': 00.00, 'light_off': 00.00, 
-            'soil_moisture': 000, 'soil_ph': 000.000}
+# empty list to send to database
+sql_list = [
+    75.2
+]
+
+# test the input of large file into database
+database(sql_dict)
 
 # print to lcd display
 # must convert to string prior to this step
 # the current LCD can only take a length of 16/line currently spaced
 # find a more efficient way to print each line
-lcd(f"Temp: {test} F    Humidity: {humidity}")
+# lcd(f"Temp: {test} F    Humidity: {humidity}")
