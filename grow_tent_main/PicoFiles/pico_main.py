1d5a57971933dba516888458ad8070e2e5673cff
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-08 00:08:53 -0500

    save and re-commit

--- grow_tent_main/PicoFiles/pico_main.py
+++ grow_tent_main/PicoFiles/pico_main.py
@@ -1,19 +1,21 @@
 # receiver.py / Tx/Rx => Tx/Rx
+# connection between Raspberry Pi and Pico
+
 import os
 import machine
 from time import sleep
 from main import from_pi
 
+# port to establish connection
 uart = machine.UART(0, 115200)
 
-b = None
-msg = ""
-
+# to notify user that the program is running
+# this will be moved into loop boody once program format is complete
 print("*****Program running*****")
 
 while True:
     print(".")
     sleep(1)
     if uart.any():
-        x = from_pi()
-        uart.write(str(x))
+        x = from_pi()  # this function will house all of the sensor data
+        uart.write(str(x))  # only know how to send strings through UART connection
--- grow_tent_main/main.py
+++ grow_tent_main/main.py
@@ -1,12 +1,32 @@
 # main control for monitoring grow
 
 import grow_tent_main
-import grow_tent_main.TempHumiditySensor
+import grow_tent_main.PicoFiles.TempHumiditySensor
 from grow_tent_main.views.LcdDisplay import lcd
 from grow_tent_main.MySQL.mysql_main import database
+import time
+import serial
 
-test = str((grow_tent_main.TempHumiditySensor.temp_f))
-humidity = str((grow_tent_main.TempHumiditySensor.hum))
+
+ser = serial.Serial(
+  port='/dev/ttyS0', # Change this according to connection methods, e.g. /dev/ttyUSB0
+  baudrate = 115200,
+  parity=serial.PARITY_NONE,
+  stopbits=serial.STOPBITS_ONE,
+  bytesize=serial.EIGHTBITS,
+  timeout=1
+)
+
+# loop to collect values throughout the day
+#  add try / except statements to prevent crash
+while True:
+    print("Gathering Readings.....")
+    ser.write('start'.encode('utf-8'))
+    time.sleep(2)
+    from_pico = ser.readline()
+    decode_pico = from_pico.decode('utf-8')
+    print(decode_pico)  # a for loop is needed here to strip and convert values to INT
+    time.sleep(2)
 
 # empty list to send to database
 sql_list = [
