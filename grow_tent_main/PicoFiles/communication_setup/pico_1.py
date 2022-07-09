6a4c2a252958d241ca13f3f43e020491f8249cc0
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-09 02:42:14 -0500

    work on gathering data from one Pico

--- grow_tent_main/PicoFiles/communication_setup/pico_1.py
+++ grow_tent_main/PicoFiles/communication_setup/pico_1.py
@@ -3,19 +3,25 @@
 
 import os
 import machine
-from time import sleep
-from main import from_pi
+import utime
+
 
 # port to establish connection
 uart = machine.UART(0, 115200)
+uart_1 = machine.UART(1, 115200)
+
+# GPIO pin to pico_2
+start = machine.Pin(15, machine.Pin.OUT)
 
 # to notify user that the program is running
 # this will be moved into loop boody once program format is complete
 print("*****Program running*****")
 
 while True:
-    print(".")
-    sleep(1)
+    print("waiting")
+    utime.sleep(1)
     if uart.any():
-        x = 5  # this function will house all of the sensor data
-        uart.write(str(x))  # only know how to send strings through UART connection
+        start.value(1)
+        utime.sleep(5)
+        received_byte = uart.read(20)
+        uart.write(str(received_byte))  # only know how to send strings through UART connection
