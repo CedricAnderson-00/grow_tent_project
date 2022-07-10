5c75e32b566c70c24e8cf605820c27fbf153eb18
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-10 14:29:40 -0500

    finish light_sensor.py module

--- grow_tent_main/PicoFiles/light_sensor.py
+++ grow_tent_main/PicoFiles/light_sensor.py
@@ -11,11 +11,16 @@ def readLight(pin1, pin2):
        """
       
     photoRes = ADC(Pin(pin1))
-    photoRes2 = ADC(Pin(pin2))  # write logic to compare values
+    # photoRes2 = ADC(Pin(pin2))  # write logic to compare values
     light = photoRes.read_u16()  # reading 16bits
+    # light2 = photoRes2.read_u16()  
     
     # find ideal lighting values and adjust to required needs
-    light = round(light/65535*100,2) 
+    light = round(light/65535*100,2)
+    # light_total = light2 - light
+    # if light_total < 10:
+    #     x = round(light/65535*100,2)  # at this point both sensors are good. Pick any
+    #     return x
     return light
 
 
