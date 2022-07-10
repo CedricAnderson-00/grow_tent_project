3762d8c6d2e46927a3358a4e6fbd381f250ed134
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-10 16:49:51 -0500

    integrate into main.py and add exception

--- grow_tent_main/PicoFiles/TempHumiditySensor.py
+++ grow_tent_main/PicoFiles/TempHumiditySensor.py
@@ -6,26 +6,33 @@ import dht
 
 # function to return values
 def get_temp_hum():
-    # remember to change sensor if using one other than an Inland DHT11
-    sensor = dht.DHT11(Pin(28))
-    sensor2 = dht.DHT11(Pin(27)) 
+    try:
+        """function that is initiated from main.py to receive that average
+        of the two temp/humidity sensors in tent
+        """
+        
+        # remember to change sensor if using one other than an Inland DHT11
+        sensor = dht.DHT11(Pin(28))
+        sensor2 = dht.DHT11(Pin(27)) 
 
-    # values of sensor 1  
-    sensor.measure()
-    temp = sensor.temperature()
-    temp_f = temp * (9 / 5) + 32
-    hum = sensor.humidity()
+        # values of sensor 1  
+        sensor.measure()
+        temp = sensor.temperature()
+        temp_f = temp * (9 / 5) + 32
+        hum = sensor.humidity()
 
-    # values of sensor 2
-    sensor2.measure()
-    temp2 = sensor2.temperature()
-    temp_f2 = temp * (9 / 5) + 32
-    hum2 = sensor2.humidity()
+        # values of sensor 2
+        sensor2.measure()
+        temp2 = sensor2.temperature()
+        temp_f2 = temp * (9 / 5) + 32
+        hum2 = sensor2.humidity()
 
-    # find tent average
-    avg_temp_f = (temp_f + temp_f2) / 2
-    avg_temp_c = (temp + temp2) / 2
-    avg_hum = (hum + hum2) / 2
+        # find tent average
+        avg_temp_f = (temp_f + temp_f2) / 2
+        avg_temp_c = (temp + temp2) / 2
+        avg_hum = (hum + hum2) / 2
+    except OSError:  # catches this error if sensor if faulty. Prevents program from crashing
+        return 0
     
     return avg_temp_c, avg_temp_f, avg_hum
 
--- grow_tent_main/PicoFiles/pico_main.py
+++ grow_tent_main/PicoFiles/pico_main.py
@@ -5,6 +5,7 @@ import os
 import machine
 from time import sleep
 from grow_tent_main.PicoFiles.TempHumiditySensor import get_temp_hum
+from grow_tent_main.PicoFiles.light_sensor import readLight
 
 # to notify user that the program is running
 # this will be moved into loop boody once program format is complete
@@ -18,22 +19,33 @@ while True:
     # counter to limit inner loop iterations
     counter = 0
     
+    # flash led so user knows system is monitoring
+    system_led = machine.Pin(25, machine.Pin.OUT)
+    system_led.toggle()
+    
     # placing the port here refreshes the port value each iteration
     uart = machine.UART(0, 115200)
     
     # nested loop to keep program running after displaying values
+    # Onboard LED flashes to let user know system is monitoring
     while counter == 0:
-        print(".")
-        sleep(1)
+        system_led.toggle()
+        sleep(.1)
+        if readLight(26) >= 50:
+            pass
+        else:
+            pass
         y = ''
-        # loop that will get values
         # transfer values in tent state to master Pi
-        if uart.any() == 5:
-            x = get_temp_hum()
+        # no call to functions here. will just read values from varaibles
+        if uart.any() == 1:
             counter +=1
-            y = [12345678901234]  # this simulates a list to pass to UART connection
-            # while loop to send values and then wait for a response back to exit loop
             while True:
                 uart.write(str(x).encode('utf-8'))
                 sleep(0.01)  # this depends on how much data is sent
                 break
+        
+        # send data of second plant
+        elif uart.any() == 2:
+            print("second plant")
+            break
