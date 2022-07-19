5b9b3fd1f70dac883ceb2646e4387cd3c409cfad
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-19 15:54:25 -0500

    add auto widow sql file

---
+++ grow_tent_main/MySQL/SQLFiles/auto_widow.sql
@@ -0,0 +1,30 @@
+INSERT INTO plants (
+    plant_name,
+    exp_yield,
+    blend,
+    min_height,
+    max_height,
+    cbd_content,
+    climate,
+    medical_usage,
+    genes,
+    supplier,
+    flowering_time_min,
+    flowering_time_max,
+    arrival_date
+  )
+VALUES (
+    'Auto Widow',
+    200,
+    'Indica Dominant Ruderalis',
+    60,
+    100,
+    '<1%',
+    'Warm Arid',
+    'Depression, Anxiety, PTSD, Stress, Insomnia',
+    'White Widow x Ruderalis',
+    'MSNL UK',
+    NULL,
+    8,
+    '2022-07-6'
+  );)
