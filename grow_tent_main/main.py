53e7002163380d6f581868306c5a99e66241452e
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-19 15:25:39 -0500

    fix readability

--- grow_tent_main/main.py
+++ grow_tent_main/main.py
@@ -7,12 +7,12 @@ def receive_from_Pico(value):
        Passes the length value to Pi Pico to send the corresponding plant values"""
     
     ser = serial.Serial(
-    port='/dev/ttyS0',  # Change this according to connection methods, e.g. /dev/ttyUSB0
+    port = '/dev/ttyS0',  # Change this according to connection methods, e.g. /dev/ttyUSB0
     baudrate = 115200,
-    parity=serial.PARITY_NONE,
-    stopbits=serial.STOPBITS_ONE,
-    bytesize=serial.EIGHTBITS,
-    timeout=1
+    parity = serial.PARITY_NONE,
+    stopbits = serial.STOPBITS_ONE,
+    bytesize = serial.EIGHTBITS,
+    timeout = 1
     )
     
     print("Gathering data.....")    
