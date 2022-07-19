d8bdce73619c50b126cc230107a18313f7d98f74
Cedric Anderson <cedric.anderson618@gmail.com>
2022-07-19 16:01:27 -0500

    update docstring

--- grow_tent_main/views/LcdDisplay.py
+++ grow_tent_main/views/LcdDisplay.py
@@ -12,8 +12,10 @@ I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
 def lcd(value):
+    """Receives any value to display on LCD"""
+    
     #Test function for verifying basic functionality
-    i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)
+    i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)  # can add multiple LCDs by changing serial ID (0)
     lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)    
     lcd.putstr(value)
     
