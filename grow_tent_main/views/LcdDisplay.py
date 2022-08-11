0d9b54087a503f511d0a0ffade96fc28ad7e238c
Cedric Anderson <cedric.anderson618@gmail.com>
2022-08-11 14:49:01 -0500

    modify lcd to receive hex and value

--- grow_tent_main/views/LcdDisplay.py
+++ grow_tent_main/views/LcdDisplay.py
@@ -7,17 +7,17 @@ from machine import I2C
 from lcd_api import LcdApi
 from pico_i2c_lcd import I2cLcd
 
-I2C_ADDR     = 0x27  # be sure to run the sample code in /FindI2CSerial.py to find device address. Convert value to hex
+# lcd properties
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
-def lcd(value):
-    """Receives any value to display on LCD"""
+def lcd(hex_address, value):
+    """Receives a hex address and a value to display"""
     
-    #Test function for verifying basic functionality
-    i2c = I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400000)  # can add multiple LCDs by changing serial ID (0)
-    lcd = I2cLcd(i2c, I2C_ADDR, I2C_NUM_ROWS, I2C_NUM_COLS)    
+    # display value
+    i2c = I2C(0, sda=machine.Pin(8), scl=machine.Pin(9), freq=400000)  # can listen on different i2c pin by changing <0>
+    lcd = I2cLcd(i2c, hex_address, I2C_NUM_ROWS, I2C_NUM_COLS)    
     lcd.putstr(value)
     
     return
-    
+   
