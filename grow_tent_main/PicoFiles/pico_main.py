38394a526bfeefb12bc2eb66f6d4b721023f5d38
Cedric Anderson <cedric.anderson618@gmail.com>
2022-08-14 01:43:43 -0500

    fix toggle switch bug

--- grow_tent_main/PicoFiles/pico_main.py
+++ grow_tent_main/PicoFiles/pico_main.py
@@ -7,6 +7,67 @@ import _thread
 
 
 # functions
+def main_body(switch):
+    
+    global low_hum, low_temp_c, low_temp_f, light_time_on, light_time_off, pump_one_total, pump_two_total, pump_three_total, send_temp_c, send_hum, send_temp_f, system_monitoring
+    
+    while True:
+    
+        # flash led so user knows system is monitoring
+        system_led = Pin(25, Pin.OUT)
+        system_led.value(1)
+        system_monitoring = True
+        water_plants(0)
+        light_controller(0)
+        display_lcd(0)
+        while switch.value() == 1:
+            if toggle_five.value() == 1:
+                water_plants(0)  
+            uart = UART(0, 115200)
+            system_led.toggle()
+            x = get_temp_hum()
+            send_temp_c = x[0]
+            send_temp_f = x[1]
+            send_hum = x[2]
+            
+            # logic to get low and high values
+            if send_hum < low_hum:
+                low_hum = send_hum
+                
+            if send_temp_c < low_temp_c:
+                low_temp_c = send_temp_c
+                
+            if send_temp_f < low_temp_f:
+                low_temp_f = send_temp_f
+                
+            # get soil readings    
+            soil1 = soil_sensor_one()
+            soil2 = soil_sensor_two()
+            soil3 = soil_sensor_three()
+
+            # transfer values in tent state to master Pi
+            if uart.any() == 1:
+                plant = 1, send_temp_f, send_temp_c, light_time_on, light_time_off, send_hum, soil1, pump_one_total
+                uart.write(str(plant).encode('utf-8'))
+                sleep(0.01)  # this depends on how much data is sent
+                pump_one_total = 0
+                
+            elif uart.any() == 2:
+                plant = 2, send_temp_f, send_temp_c, light_time_on, light_time_off, send_hum, soil2, pump_two_total
+                uart.write(str(plant).encode('utf-8'))
+                sleep(0.01)  # this depends on how much data is sent
+                pump_two_total = 0
+
+            elif uart.any() == 3:
+                plant = 3, send_temp_f, send_temp_c, light_time_on, light_time_off, send_hum, soil3, pump_three_total
+                uart.write(str(plant).encode('utf-8'))
+                sleep(0.01)  # this depends on how much data is sent
+                pump_three_total = 0
+                
+        return
+                
+
+
 def phase_switch_thread():
     """Function that creates a thread to monitor toggle switches to change grow parameters"""
     
@@ -35,10 +96,9 @@ def light_controller(t):
     
     if toggle_one.value() == 1 | toggle_two.value() == 1:
         tent_light_control.value(1)
-        hour_timer = Timer(period=3_600_000, mode=Timer.PERIODIC, callback=light_controller)  # 1 hour in milliseconds
+        hour_timer = Timer(period=3_600_000, mode=Timer.ONE_SHOT, callback=light_controller)  # 1 hour in milliseconds
         light_time_on +=1  
     elif toggle_three.value() == 1:
-        Timer.deinit(hour_timer)
         tent_light_control.value(0)
         veg_timer_on = Timer(period=43_200_000, mode=Timer.ONE_SHOT, callback=lights_on)  # 12 hours in ms
     elif toggle_four.value() == 1:
@@ -117,6 +177,8 @@ def water_plants(t):
         pump_one.value(0)
         pump_two.value(0)
         pump_three.value(0)
+    elif toggle_five.value() == 1:
+        print("manual watering")
     
     # recursion
     water_timer = Timer(period=172_800_000, mode=Timer.ONE_SHOT, callback=fertilizer)  # timer to water every other day
@@ -186,15 +248,14 @@ def display_lcd(t):
     lcd_three = 0x26  # humidity
     lcd_four = 0x27  # lights
     
-    global display_hum, display_c, display_f, low_hum, low_temp_f, low_temp_c, light_time_on, light_time_off, grow_cycle
+    global send_hum, send_temp_c, send_temp_f, low_hum, low_temp_f, low_temp_c, light_time_on, light_time_off, grow_cycle, display_timer
     
-    lcd(lcd_one, str(display_f), str(low_temp_f), 1)
-    lcd(lcd_two, str(display_c), str(low_temp_c), 2)
-    lcd(lcd_three, str(display_hum), str(low_hum), 3) 
+    lcd(lcd_one, str(send_temp_f), str(low_temp_f), 1)
+    lcd(lcd_two, str(send_temp_c), str(low_temp_c), 2)
+    lcd(lcd_three, str(send_hum), str(low_hum), 3) 
     lcd(lcd_four, str(light_time_on), str(light_time_off), 4, str(grow_cycle)) 
-        
-    return
     
+    display_timer 
 
 # toggle switch GPIO
 toggle_one = Pin(16, Pin.IN, Pin.PULL_DOWN)
@@ -209,6 +270,7 @@ phase_one = False
 phase_two = False
 phase_three = False
 phase_four = False
+system_monitoring = False
     
 # timer values 
 light_time_on = 0000
@@ -229,11 +291,12 @@ fert_one = Pin(20, Pin.OUT)
 fert_two = Pin(21, Pin.OUT)
 fert_three = Pin(22, Pin.OUT)
 stir_plate = Pin(12, Pin.OUT)
+send_hum = 0
+send_temp_c = 0
+send_temp_f = 0
 
-# variables to start water and light cycles  
-water_plants(0)
-light_controller(0)
-display_timer = Timer(period=60_000, mode=Timer.PERIODIC, callback=display_lcd)  # updates lcd(s) every minute
+# variables to start water and light cycles
+display_timer = Timer(period=30_000, mode=Timer.ONE_SHOT, callback=display_lcd) 
 _thread.start_new_thread(phase_switch_thread, ())
 
 # low values
@@ -253,48 +316,20 @@ while True:
     system_led = Pin(25, Pin.OUT)
     system_led.value(1)
     while True:  # change to number of plants
-        if toggle_one.value()| toggle_two.value() | toggle_three.value() | toggle_four.value() == 1:
-            water_plants(0)
-            light_controller(0)
-        elif toggle_five.value() == 1:
-            pass
-        uart = UART(0, 115200)
-        system_led.toggle()
-        x = get_temp_hum()
-        send_temp_c = x[0]
-        send_temp_f = x[1]
-        send_hum = x[2]
+        while toggle_one.value() == 1:
+            print("seedling phase")
+            system_monitoring = True
+            main_body(toggle_one)
+        while toggle_two.value() == 1:
+            print("vegetation stage")
+            system_monitoring = True
+            main_body(toggle_two)
+        while toggle_three.value() == 1:
+            print("flowering phase")
+            system_monitoring = True
+            main_body(toggle_three)
+        while toggle_four.value() == 1:
+            print("harvesting")
+            system_monitoring = True
+            main_body(toggle_four)
         
-        # logic to get low and high values
-        if send_hum < low_hum:
-            low_hum = send_hum
-            
-        if send_temp_c < low_temp_c:
-            low_temp_c = send_temp_c
-            
-        if send_temp_f < low_temp_f:
-            low_temp_f = send_temp_f
-            
-        # get soil readings    
-        soil1 = soil_sensor_one()
-        soil2 = soil_sensor_two()
-        soil3 = soil_sensor_three()
-
-        # transfer values in tent state to master Pi
-        if uart.any() == 1:
-            plant = 1, send_temp_f, send_temp_c, light_time_on, light_time_off, send_hum, soil1, pump_one_total
-            uart.write(str(plant).encode('utf-8'))
-            sleep(0.01)  # this depends on how much data is sent
-            pump_one_total = 0
-              
-        elif uart.any() == 2:
-            plant = 2, send_temp_f, send_temp_c, light_time_on, light_time_off, send_hum, soil2, pump_two_total
-            uart.write(str(plant).encode('utf-8'))
-            sleep(0.01)  # this depends on how much data is sent
-            pump_two_total = 0
-
-        elif uart.any() == 3:
-            plant = 3, send_temp_f, send_temp_c, light_time_on, light_time_off, send_hum, soil3, pump_three_total
-            uart.write(str(plant).encode('utf-8'))
-            sleep(0.01)  # this depends on how much data is sent
-            pump_three_total = 0
