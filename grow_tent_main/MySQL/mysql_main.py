99a8a04fde98e9f7755f6b17c76e7056edd890ef
Cedric Anderson <cedric.anderson618@gmail.com>
2023-04-10 17:42:36 -0500

    add new data values.

--- grow_tent_main/MySQL/mysql_main.py
+++ grow_tent_main/MySQL/mysql_main.py
@@ -18,15 +18,14 @@ def database(list):
             plant_id = list[0]
             temp_f = list[1]
             temp_c = list[2]
-            humidity = list[5]
-            daily_water = list[7]
-            fert_water = list[8]
-            light_on = list[3]
-            light_off = list[4]
-            soil_moisture = list[6]
+            humidity = list[3]
+            light_time_on = list[6]
+            system_timer = list[4]
+            daily_water = list[5]
+            light_time_off = list[7]
             program_time = datetime.now()
-            test_data = [(temp_f, temp_c, humidity, daily_water, fert_water, light_on, light_off, 
-                          soil_moisture, program_time, plant_id)]
+            test_data = [(temp_f, temp_c, humidity, daily_water, light_time_on, light_time_off, system_timer, 
+                        program_time, plant_id)]
             
             # the <%s> tells python that more data will come after the execution of that line
             insert_main_data = """
