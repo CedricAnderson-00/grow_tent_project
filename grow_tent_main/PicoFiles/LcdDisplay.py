6be259fb396871aabe0368a747dac3faeaf41c42
Falconview314 <100259965+Falconview314@users.noreply.github.com>
2023-05-03 10:44:43 -0500

    add error counter.

--- grow_tent_main/PicoFiles/LcdDisplay.py
+++ grow_tent_main/PicoFiles/LcdDisplay.py
@@ -5,20 +5,21 @@ from pico_i2c_lcd import I2cLcd
 I2C_NUM_ROWS = 2
 I2C_NUM_COLS = 16
 
-def lcd(hex_address, value_1, value_2, value_3, value_4, name):
+def lcd(hex_address, value_1, value_2, value_3, value_4, value_5, name):
     """Receives a hex address and a value to display"""
       
     # display value
     i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)  # can listen on different i2c pin by changing <0>
     lcd = I2cLcd(i2c, hex_address, I2C_NUM_ROWS, I2C_NUM_COLS)
     if name == 1:    
-        lcd.putstr(f"{value_1:.4} F  {value_3:.4} C \n{value_2:.4} RH: {value_2} HRS: {value_4}")
+        lcd.putstr(f"{value_1:.4} F  {value_3:.4} C  {value_5} \nRH: {value_2:.4} HRS: {value_4}")
     elif name == 2:
-        lcd.putstr(f"Temperature C \nH: {value_1:.4} L: {value_2:.4}")
+        lcd.putstr(f"Temperature C \nH: {value_1:.4} L: {value_2:.4} {value_5}")
     elif name == 3:
         lcd.putstr(f"Humidity \nH: {value_1:.4} L: {value_2:.4}")
     elif name == 4:
         lcd.putstr(f"Light On: {value_1}h\nOff: {value_2}h")
     
     return
+
 
