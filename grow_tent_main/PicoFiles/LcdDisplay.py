6b5b22e9305e81d9b7c0871a5ce3990388b688a9
Cedric Anderson <cedric.anderson618@gmail.com>
2023-07-29 13:34:47 -0500

    change precision amounts

--- grow_tent_main/PicoFiles/LcdDisplay.py
+++ grow_tent_main/PicoFiles/LcdDisplay.py
@@ -12,7 +12,7 @@ def lcd(hex_address, value_1, value_2, value_3, value_4, value_5, name):
     i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)  # can listen on different i2c pin by changing <0>
     lcd = I2cLcd(i2c, hex_address, I2C_NUM_ROWS, I2C_NUM_COLS)
     if name == 1:    
-        lcd.putstr(f"{value_1:.4} F  {value_3:.4} C  {value_5} \nRH: {value_2:.4} HRS: {value_4}")
+        lcd.putstr(f"{value_1}F  {value_3}C  {value_5} \nRH: {value_2} HRS: {value_4}")
     elif name == 2:
         lcd.putstr(f"Temperature C \nH: {value_1:.4} L: {value_2:.4} {value_5}")
     elif name == 3:
@@ -21,5 +21,6 @@ def lcd(hex_address, value_1, value_2, value_3, value_4, value_5, name):
         lcd.putstr(f"Light On: {value_1}h\nOff: {value_2}h")
     
     return
+
 
 
